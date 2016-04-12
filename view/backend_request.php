2419ce9f92d79575edc77ca3cfd2eb3b5b7bcb11
Kay Wilhelm Mähler <KWMSources@users.noreply.github.com>
2016-04-12 10:22:23 +0200

    Antragsfeature add Posttype Display

---
+++ view/backend_request.php
@@ -0,0 +1,204 @@
+<?php
+
+/**
+ * Description of backend_requestr
+ *
+ * @author KWM
+ */
+class backend_request {
+	public static function maskHandler() {
+		add_meta_box("io_request_info_mb", "Informationen", array("backend_request", "metaInfo"), Request_Control::POST_TYPE, "normal", "default");
+		add_meta_box("io_request_action_mb", "Aktion", array("backend_request", "metaAction"), Request_Control::POST_TYPE, "normal", "default");
+	}
+	
+	public static function maskInfo($post) {
+		$request = new Request($post->ID);
+		$request_art = Request_Factory::getRequest($request->art, $post->ID);
+		
+		?>
+
+<table border="0" cellpadding="5" cellspacing="0" width="100%">
+	<tr>
+		<td width="40%">Berechtigungsart:</td>
+		<td width="60%"><b><?php echo $request->art; ?></b></td>
+	</tr>
+	<tr>
+		<td width="40%">Steller*in:</td>
+		<td width="60%"><b><?php echo $request->steller_in; ?></b></td>
+	</tr>
+	<tr>
+		<td width="40%">F&uuml;r:</td>
+		<td width="60%"><b><?php echo $request_art->getObject()->name; ?></b></td>
+	</tr>
+	<tr>
+		<td width="40%">Status:</td>
+		<td width="60%"><b><?php echo $request->status; ?></b></td>
+	</tr>
+</table>
+
+<?php
+	}
+	
+	public static function maskAction($post) {
+		$request = new Request($post->ID);
+		wp_nonce_field('io_request_action', 'io_request_action_nonce');
+		
+		if($request->status == "Gestellt") {
+			?>
+
+<input type="radio" name="io_request_status" value="annahme"> Antrag annehmen
+<br><br><br>
+<input type="radio" name="io_request_status" value="ablehnung"> Antrag ablehnen
+
+<?php
+		} else {
+			?><b>Antrag wurde bereits <?php echo $request->status; ?>.</b><?php
+		}
+	}
+	
+	public static function maskSave($post_id) {
+		if(current_user_can('administrator')) {
+			if( !isset($_POST['io_request_action_nonce']) || 
+				!wp_verify_nonce($_POST['io_request_action_nonce'], 'io_request_action') || 
+				defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ||
+				empty($_POST['io_request_status'])) {
+				return;
+			}
+			
+			if($_POST['io_request_status'] == "annahme") {
+				Request_Control::approve($post_id);
+			} else if($_POST['io_request_status'] == "ablehnung") {
+				Request_Control::reject($post_id);
+			}
+			
+			/*
+			 * TODO: SAVE NACHRICHT!!!
+			 */
+		}
+	}
+	
+	public static function menu() {
+		if(current_user_can('administrator')) {
+			if(Request_Control::count() > 0) {
+				global $menu;
+				foreach($menu AS $key => $value) {
+					if($menu[$key][2] == "edit.php?post_type=io_request") {
+						$menu[$key][0] = $menu[$key][0] . ' <span class="update-plugins ' . Request_Control::count() . '"><span class="plugin-count">' . Request_Control::count() . '</span></span>';
+						break;
+					}
+				}
+			}
+		}
+		return;
+	}
+	
+	public static function column($columns) {
+		return array_merge($columns, array('io_request_status' => 'Status'));
+	}
+	
+	public static function maskColumn($column, $post_id) {
+		if($column == 'io_request_status') {
+			echo (new Request($post_id))->status;
+		}
+	}
+	
+	public static function orderby($vars) {
+		if($vars['post_type'] == Request_Control::POST_TYPE && $vars['orderby'] == 'Status') {
+			$vars = array_merge($vars, array(
+				'meta_key'	=> 'io_request_status',
+				'orderby'	=> 'meta_value'
+			));
+		}
+		return $vars;
+	}
+	
+	public static function maskFiltering() {
+		$screen = get_current_screen();
+		if($screen->post_type == Request_Control::POST_TYPE) {
+			$groups = Group_Control::getValues();
+			$permissions = Permission_Control::getValues();
+			$users = User_Control::getValues();
+			
+			?><table border="0" cellpadding="0" cellspacing="0" width="100%">
+	<tr>
+		<td>
+			<?php
+			$title = "Arten";
+			$values = Request_Factory::getValues();
+			$name = "io_request_art";
+			$selected = array();
+			if(isset($_POST['io_request_art'])) {
+				$selected = $_POST['io_request_art'];
+			}
+			include '../wp-content/plugins/igeloffice/templates/backend/filterSelect.php';
+			?>
+		</td>
+		<td>
+<?php		
+			$title = "Status";
+			$values = array("Angenommen", "Abgelehnt", "Gestellt");
+			$name = "io_request_status";
+			$selected = array();
+			if(isset($_POST['io_request_status'])) {
+				$selected = $_POST['io_request_status'];
+			}
+			include '../wp-content/plugins/igeloffice/templates/backend/filterSelect.php';
+			?>
+		</td>
+		<td>
+<?php		
+			$title = "Gruppen";
+			$values = $groups;
+			$name = "io_request_requested_id_groups";
+			$selected = array();
+			if(isset($_POST['io_request_requested_id_groups'])) {
+				$selected = $_POST['io_request_requested_id_groups'];
+			}
+			include '../wp-content/plugins/igeloffice/templates/backend/filterSelect.php';
+			?>
+		</td>
+		<td>
+<?php		
+			$title = "Berechtigungen";
+			$values = $permissions;
+			$name = "io_request_requested_id_permissions";
+			$selected = array();
+			if(isset($_POST['io_request_requested_id_permissions'])) {
+				$selected = $_POST['io_request_requested_id_permissions'];
+			}
+			include '../wp-content/plugins/igeloffice/templates/backend/filterSelect.php';
+			?>
+		</td>
+		<td>
+<?php		
+			$title = "Steller*in";
+			$values = $users;
+			$name = "io_request_steller_in";
+			$selected = array();
+			if(isset($_POST['io_request_steller_in'])) {
+				$selected = $_POST['io_request_steller_in'];
+			}
+			include '../wp-content/plugins/igeloffice/templates/backend/filterSelect.php';
+			?>
+		</td>
+	</tr>
+</table>
+<?php
+		}
+	}
+	
+	public static function filtering($query) {
+		$_POST['io_request_requested_id'] = array();
+		
+		foreach($_POST['io_request_requested_id_groups'] AS $group) {
+			$_POST['io_request_requested_id'][] = $group;
+		}
+		
+		foreach($_POST['io_request_requested_id_permissions'] AS $permission) {
+			$_POST['io_request_requested_id'][] = $permission;
+		}
+		
+		$names = array('io_request_art', 'io_request_status', 'io_request_requested_id', 'io_request_steller_in');
+		return io_filter($query, $names, Request_Control::POST_TYPE);
+	}
+}
