461d5faab7197db0a0d282467c2dda52739f1559
Kay Wilhelm Mähler <mail@kay-wilhelm.de>
2016-07-22 15:07:26 +0200

    Refactor Post-Attribut mailStandard

--- permission/mailStandard/mailStandard_view.php
+++ permission/mailStandard/mailStandard_view.php
@@ -8,6 +8,12 @@
 class mailStandard_view {
 	private static $user;
 	
+	private static $POST_ATTRIBUT_MAIL_FORWARD_SUBMIT = 'io_mailForward_submit';
+	private static $POST_ATTRIBUT_MAIL_STANDARD_NONCE = 'io_mailStandard_nonce';
+	private static $POST_ATTRIBUT_MAIL = 'io_mail';
+	private static $POST_ATTRIBUT_MAIL_FORWARD = 'io_mailForward';
+	private static $MAIL_STANDARD_NONCE = 'io_mailStandard';
+	
 	public static function maskHandler() {
 		self::$user = new mailStandard_model(get_current_user_id());
 		
@@ -17,7 +23,7 @@ class mailStandard_view {
 		
 		include 'wp-content/plugins/igeloffice/permission/mailStandard/templates/header.php';
 		
-		wp_nonce_field('io_mailStandard', 'io_mailStandard_nonce');
+		wp_nonce_field(self::$MAIL_STANDARD_NONCE, self::$POST_ATTRIBUT_MAIL_STANDARD_NONCE);
 		
 		if(self::$user->isMailPermitted) {
 			self::maskMail();
@@ -30,47 +36,47 @@ class mailStandard_view {
 	}
 	
 	public static function maskMail() {
-		if(isset($_POST['io_mailForward_submit'])) {
+		if(isset($_POST[self::$POST_ATTRIBUT_MAIL_FORWARD_SUBMIT])) {
 			self::mailExecution();
 		}
 		include 'wp-content/plugins/igeloffice/permission/mailStandard/templates/mail.php';
 	}
 	
 	public static function maskMailForward() {
-		if(isset($_POST['io_mailForward_submit'])) {
+		if(isset($_POST[self::$POST_ATTRIBUT_MAIL_FORWARD_SUBMIT])) {
 			self::mailForwardExecution();
 		}
 		include 'wp-content/plugins/igeloffice/permission/mailStandard/templates/mailForward.php';
 	}
 	
 	public static function mailExecution() {
-		if( !isset($_POST['io_mailStandard_nonce']) || 
-			!wp_verify_nonce($_POST['io_mailStandard_nonce'], 'io_mailStandard') || 
+		if( !isset($_POST[self::$POST_ATTRIBUT_MAIL_STANDARD_NONCE]) || 
+			!wp_verify_nonce($_POST[self::$POST_ATTRIBUT_MAIL_STANDARD_NONCE], self::$MAIL_STANDARD_NONCE) || 
 			defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
 			return;
 		}
 		
 		$mailStandard_model = new mailStandard_model(get_current_user_id());
 		
-		if(isset($_POST['io_mail']) && $_POST['io_mail'] == "true" && !isset($_POST['io_mailForward']) && !$mailStandard_model->useMail) {
+		if(isset($_POST[self::$POST_ATTRIBUT_MAIL]) && $_POST[self::$POST_ATTRIBUT_MAIL] == "true" && !isset($_POST[self::$POST_ATTRIBUT_MAIL_FORWARD]) && !$mailStandard_model->useMail) {
 			mailStandard_control::setMail(get_current_user_id());
-		} else if(!isset($_POST['io_mail']) && $mailStandard_model->useMail) {
+		} else if(!isset($_POST[self::$POST_ATTRIBUT_MAIL]) && $mailStandard_model->useMail) {
 			mailStandard_control::delMail(get_current_user_id());
 		}
 	}
 	
 	public static function mailForwardExecution() {
-		if( !isset($_POST['io_mailStandard_nonce']) || 
-			!wp_verify_nonce($_POST['io_mailStandard_nonce'], 'io_mailStandard') || 
+		if( !isset($_POST[self::$POST_ATTRIBUT_MAIL_STANDARD_NONCE]) || 
+			!wp_verify_nonce($_POST[self::$POST_ATTRIBUT_MAIL_STANDARD_NONCE], self::$MAIL_STANDARD_NONCE) || 
 			defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
 			return;
 		}
 		
 		$mailStandard_model = new mailStandard_model(get_current_user_id());
 		
-		if(isset($_POST['io_mailForward']) && $_POST['io_mailForward'] == "true" && !isset($_POST['io_mail']) && !$mailStandard_model->useMailForward) {
+		if(isset($_POST[self::$POST_ATTRIBUT_MAIL_FORWARD]) && $_POST[self::$POST_ATTRIBUT_MAIL_FORWARD] == "true" && !isset($_POST[self::$POST_ATTRIBUT_MAIL]) && !$mailStandard_model->useMailForward) {
 			mailStandard_control::setMailForward(get_current_user_id());
-		} else if(!isset($_POST['io_mailForward']) && $mailStandard_model->useMailForward) {
+		} else if(!isset($_POST[self::$POST_ATTRIBUT_MAIL_FORWARD]) && $mailStandard_model->useMailForward) {
 			mailStandard_control::delMailForward(get_current_user_id());
 		}
 	}
